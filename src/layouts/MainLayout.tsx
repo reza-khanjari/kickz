286b48c2544d33f9085fcca56ec4931bf3bf91ff
reza-khanjari <light755646@gmail.com>
2026-08-29 17:51:47 +0330

    Add ScrollRestoration component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,13 @@
-import {  Outlet } from "react-router";
+import {  Outlet, ScrollRestoration } from "react-router";
 import Header from "./Header";
 import BackToTop from "@/ui/BackToTop";
-import Skeleton from "@/ui/Skeleton";
+
 
 function MainLayout() {
   return (
     <>
       <BackToTop />
+      <ScrollRestoration />
       <Header />
       <main>
        
